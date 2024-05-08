44f3f516e491f438e9437f33bf942096186f4070
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-05-08 13:42:02 -0400

    Edit vscode multi editing keymaps

--- nvim/lua/plugins-vscode.lua
+++ nvim/lua/plugins-vscode.lua
@@ -9,15 +9,13 @@ return {
 
       -- vscode-multi-cursor
       vim.keymap.set({ 'n', 'x' }, 'mc', require('vscode-multi-cursor').create_cursor, { expr = true })
-      vim.keymap.set({ 'n' }, 'mcc', require('vscode-multi-cursor').cancel)
+      vim.keymap.set({ 'n' }, 'mr', require('vscode-multi-cursor').cancel)
       vim.keymap.set({ 'n', 'x' }, 'mi', require('vscode-multi-cursor').start_left)
-      vim.keymap.set({ 'n', 'x' }, 'mI', require('vscode-multi-cursor').start_left_edge)
       vim.keymap.set({ 'n', 'x' }, 'ma', require('vscode-multi-cursor').start_right)
-      vim.keymap.set({ 'n', 'x' }, 'mA', require('vscode-multi-cursor').start_right)
-      vim.keymap.set({ 'n' }, '[mc', require('vscode-multi-cursor').prev_cursor)
-      vim.keymap.set({ 'n' }, ']mc', require('vscode-multi-cursor').next_cursor)
-      vim.keymap.set({ 'n' }, 'mcs', require('vscode-multi-cursor').flash_char)
-      vim.keymap.set({ 'n' }, 'mcw', require('vscode-multi-cursor').flash_word)
+      vim.keymap.set({ 'n' }, 'mj', require('vscode-multi-cursor').next_cursor)
+      vim.keymap.set({ 'n' }, 'mk', require('vscode-multi-cursor').prev_cursor)
+      vim.keymap.set({ 'n' }, 'mfc', require('vscode-multi-cursor').flash_char)
+      vim.keymap.set({ 'n' }, 'mfw', require('vscode-multi-cursor').flash_word)
       vim.keymap.set('n', '<C-t>', 'mciw*<Cmd>nohl<CR>', { remap = true })
     end,
   },
