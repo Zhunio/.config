ba7e98cf1a1484540b25d2f40b1fd83c4e2215b9
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-05-22 12:09:41 -0400

    improve vscode multi cursor

--- nvim/lua/plugins-vscode.lua
+++ nvim/lua/plugins-vscode.lua
@@ -14,16 +14,23 @@ return {
       vim.keymap.set({ 'n', 'x' }, 'ma', require('vscode-multi-cursor').start_right)
       vim.keymap.set({ 'n' }, 'mj', require('vscode-multi-cursor').next_cursor)
       vim.keymap.set({ 'n' }, 'mk', require('vscode-multi-cursor').prev_cursor)
+      vim.keymap.set({ 'x' }, 'mh', function() require('vscode-multi-cursor').selectHighlights() end)
       vim.keymap.set({ 'n' }, 'mfc', require('vscode-multi-cursor').flash_char)
       vim.keymap.set({ 'n' }, 'mfw', require('vscode-multi-cursor').flash_word)
       vim.keymap.set('n', '<C-t>', 'mciw*<Cmd>nohl<CR>', { remap = true })
+      
+      -- 'app-grid:nth-child(1) .rollout-block-container:nth-child(1)'
     end,
   },
   {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {
+      highlight = { backdrop = false },
       modes = {
+        search = {
+          highlight = { backdrop = false }
+        },
         char = {
           highlight = { backdrop = false }
         }
